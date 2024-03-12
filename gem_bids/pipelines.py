edc7f21a0e46f6fe95611aca24370f422b292993
surbhitt <surbhit.thakur@gmail.com>
2024-03-12 10:12:50 +0530

    final

--- gem_bids/pipelines.py
+++ gem_bids/pipelines.py
@@ -2,6 +2,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import itemadapter
 import psycopg2
 
 # useful for handling different item types with a single interface
@@ -10,40 +11,42 @@ from itemadapter import ItemAdapter
 
 class GemBidsPipeline:
     def __init__(self):
-        hostname = 'localhost'
-        username = 'postgres'
-        password = 'admin' # your password
-        database = 'test'
-        self.con = psycopg2.connect(hostname=hostname, user=username, password=password, dbname=database)
+        host = 'localhost'
+        user = 'postgres'
+        passw = 'admin'
+        db = 'test'
+        self.con = psycopg2.connect(host=host, user=user, password=passw, database=db)
         self.cur = self.con.cursor()
         self.cur.execute("""
             CREATE TABLE IF NOT EXISTS bids(
                 id serial PRIMARY KEY,
                 Bid_No text,
                 Ra_No text,
-                Items int,
+                Items text,
                 Quantity int,
                 Department text,
                 Start_date date,
                 End_date date,
-                doclink text,
+                doclink text
                 )""")
 
-    def process_item(self, items, spider):
-        for item in items:
-            self.cur.execute(""" 
-            INSERT INTO bids (Bid_No, Ra_No, Items, Quantity, Department, Start_date, End_date, doclink) values (%s, %s, %s, %s, %s, %s, %s, %s)""", (
-                item["Bid_No"],
-                item["Ra_No"],
-                item["Items"],
-                item["Quantity"],
-                item["Department"],
-                item["Start_date"],
-                item["End_date"],
-                item["doclink"],
-            ))
-            self.con.commit()
-        return items
+    def process_item(self, item, spider):
+        for k in item:
+            if not item[k]:
+                item[k] = "NA"
+        self.cur.execute(""" 
+        INSERT INTO bids (Bid_No, Ra_No, Items, Quantity, Department, Start_date, End_date, doclink) values (%s, %s, %s, %s, %s, %s, %s, %s)""", (
+            item["Bid_No"][0] if len(item["Bid_No"]) else "NA",
+            item["Ra_No"][0],
+            item["Items"],
+            item["Quantity"][0],
+            item["Department"][0],
+            item["Start_date"],
+            item["End_date"],
+            item["doclink"]
+        ))
+        self.con.commit()
+        return item
 
     def close_spider(self):
         self.cur.close()
